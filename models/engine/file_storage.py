201055b463ec7b98eb3a08ed54ce903878d0eda1
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-11-22 14:53:24 -0800

    Added models/engine/file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,15 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is None:
+            return FileStorage.__objects
+        rt = {}
+        for key, value in FileStorage.__objects.items():
+            if type(value) == cls:
+                rt[key] = value
+        return rt
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -45,6 +51,13 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """Should Delete obj if from __objects if its inside"""
+        if obj is not None and type(obj) == dict \
+                and 'id' in obj.keys():
+            key = obj.to_dict()['__class__'] + '.' + str(obj.id)
+            self.all().pop(key, None)
