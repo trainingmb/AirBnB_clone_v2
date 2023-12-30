98448481d5ca8dac6c342e9f6adbec4254177444
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-12-30 13:56:37 +0000

    Added 4-number_route.py

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""Is it a Number?"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_hbnb():
+    """At the home address displays hello hbnh"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    """Adds the route for HBNB"""
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def c_is_fun(text):
+    """Display "C" followed by the value of the
+       text variable (replace underscore _ symbols
+       with a space )
+    """
+    return "C " + text.replace("_", " ")
+
+
+@app.route("/python/")
+@app.route("/python/<text>")
+def python_is_cool(text="is_cool"):
+    """Display Python, followed by the value of the
+       text variable (replacing underscores with spaces)
+    """
+    return "Python " + text.replace("_", " ")
+
+
+@app.route("/number/<int:n>")
+def is_it_number(n):
+    """Displays n is a number only if n is an integer
+    """
+    return "{} is a number".format(n)
+
+
+if __name__ == "__main__":
+    app.url_map.strict_slashes = False
+    app.run(host="0.0.0.0", port=5000)
