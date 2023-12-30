a0bfde0978cb9e6cccc00217ede7297582d36d90
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-12-30 13:36:32 +0000

    Added 2-c_route.py

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""C is Fun!"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_hbnb():
+    """At the home address displays hello hbnh"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    """Adds the route for HBNB"""
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def c_is_fun(text):
+    """Display "C" followed by the value of the
+       text variable (replace underscore _ symbols
+       with a space )
+    """
+    return "C " + text.replace("_", " ")
+
+
+if __name__ == "__main__":
+    app.url_map.strict_slashes = False
+    app.run(host="0.0.0.0", port=5000)
