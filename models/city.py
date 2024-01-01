9696677e1651565d458e759c3048eec5c208ffe7
trainingmb <118762148+trainingmb@users.noreply.github.com>
2024-01-02 02:06:20 +0300

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,30 +1,30 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
+import models
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey, Integer
 from sqlalchemy.orm import relationship
 from os import environ
 from uuid import uuid4
+from os import getenv
+import sqlalchemy
 
-s = "HBNB_TYPE_STORAGE"
-if s in environ.keys() and environ["HBNB_TYPE_STORAGE"] == "db":
-    class City(BaseModel, Base):
-        '''
-        City for SQLAlchemy
-        '''
+
+class City(BaseModel, Base):
+    '''
+    City for SQLAlchemy or JSON
+    '''
+    if models.storage_t == "db":
         __tablename__ = "cities"
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
         name = Column(String(128), nullable=False)
         places = relationship("Place", backref="cities", cascade="all,delete")
-
-        def __init__(self, **kwargs):
-            setattr(self, "id", str(uuid4()))
-            for i, j in kwargs.items():
-                setattr(self, i, j)
-else:
-    class City(BaseModel):
-        def delete(self):
-            models.storage.delete(self)
-            models.storage.save()
-        """ The city class, contains state ID and name """
+    else:
         state_id = ""
         name = ""
+
+    def __init__(self, **kwargs):
+        """
+        Initialize City
+        """
+        super().__init__(*args, **kwargs)
