2b1cc408a28c3c3588767480815493cc44119064
trainingmb <118762148+trainingmb@users.noreply.github.com>
2024-01-02 02:10:41 +0300

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,31 +1,30 @@
 #!/usr/bin/python3
 """This module defines a class User"""
+import models
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
 
-s = "HBNB_TYPE_STORAGE"
-if cs in environ.keys() and environ["HBNB_TYPE_STORAGE"] == "db":
-    class User(BaseModel, Base):
-        """User Class
+
+class User(BaseModel, Base):
+    """
+    User Class
         Attributes: email, password, first_name, last_name
-        """
-        __tablename__ = "users"
+    """
+    if models.storage_t == 'db':
+        __tablename__ = 'users'
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
         places = relationship("Place", backref="user")
         reviews = relationship("Review", backref="user")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
 
-        def __init__(self, **kwargs):
-            setattr(self, "id", str(uuid4()))
-            for i, j in kwargs.items():
-                setattr(self, i, j)
-else:
-    class User(BaseModel):
-        """This class defines a user by various attributes"""
-        email = ''
-        password = ''
-        first_name = ''
-        last_name = ''
+    def __init__(self, *args, **kwargs):
+        """Initialize user with basemodel"""
+        super().__init__(*args, **kwargs)
