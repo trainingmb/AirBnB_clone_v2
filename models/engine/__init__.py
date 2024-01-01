12d0fe8bf3ef747319646e9ec093662553755d34
trainingmb <118762148+trainingmb@users.noreply.github.com>
2024-01-02 01:36:04 +0300

    Update __init__.py

---
+++ models/engine/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""File Storage for Models"""
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+from models.user import User
+
+
+s = "HBNB_TYPE_STORAGE"
+if s in environ.keys() and environ["HBNB_TYPE_STORAGE"] == "db":
+    storage = DBStorage()
+    storage.reload()
+else:
+    storage = FileStorage()
+    storage.reload()
