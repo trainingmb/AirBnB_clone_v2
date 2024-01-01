6d2ce299cfbcd5cd02d931edce64eab0f7c80bff
trainingmb <118762148+trainingmb@users.noreply.github.com>
2024-01-02 02:33:13 +0300

    Update __init__.py

--- models/engine/__init__.py
+++ models/engine/__init__.py
@@ -6,6 +6,7 @@ from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
 from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
 from models.base_model import BaseModel
 from models.user import User
 from os import environ
