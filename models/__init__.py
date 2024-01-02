2944d00aab66e0e7d0ca9886b6d29c6a73f052b5
trainingmb <118762148+trainingmb@users.noreply.github.com>
2024-01-02 04:17:07 +0300

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -7,9 +7,11 @@ storage_t = getenv("HBNB_TYPE_STORAGE")
 storage = ""
 if storage_t == "db":
     from models.engine.db_storage import DBStorage
+    print("MYSQL DB")
     storage = DBStorage()
 else:
     from models.engine.file_storage import FileStorage
+    print("JSON DB")
     storage = FileStorage()
 
 
