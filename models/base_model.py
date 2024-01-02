207f4bc8d36cae59a4684dbd455cccef6a21f326
trainingmb <118762148+trainingmb@users.noreply.github.com>
2024-01-02 04:07:17 +0300

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -46,6 +46,7 @@ class BaseModel:
                 self.id = str(uuid.uuid4())
                 self.created_at = datetime.now()
                 self.updated_at = datetime.now()
+                del kwargs['isNew']
                 storage.new(self)
 
     def __str__(self):
