60b545061ed2a37aba2c3f07ff860954849955f4
trainingmb <118762148+trainingmb@users.noreply.github.com>
2024-01-02 03:04:38 +0300

    Update console.py

--- console.py
+++ console.py
@@ -124,6 +124,7 @@ class HBNBCommand(cmd.Cmd):
             return
         new_instance = HBNBCommand.classes[al[0]]()
         key = al[0] + "." + new_instance.id
+        print(storage.all())
         storage.save()
         new_dict = storage.all()[key]
         atts = al[2].split(" ")
